2b9b3e1f7b956ec17c001a8db2f5bbb99fe80300
qiyutao <qiyutao12@163.com>
2016-07-18 16:17:19 +0800

    Create 字符串查找.java
    对于一个给定的 source 字符串和一个 target 字符串，你应该在 source 字符串中找出 target 字符串出现的第一个位置(从0开始)。如果不存在，则返回 -1。
    
    说明
    在面试中我是否需要实现KMP算法？
    
    不需要，当这种问题出现在面试中时，面试官很可能只是想要测试一下你的基础应用能力。当然你需要先跟面试官确认清楚要怎么实现这个题。
    样例
    如果 source = "source" 和 target = "target"，返回 -1。
    
    如果 source = "abcdabcdefg" 和 target = "bcd"，返回 1。

---
+++ 字符串查找.java
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * Returns a index to the first occurrence of target in source,
+     * or -1  if target is not part of source.
+     * @param source string to be scanned.
+     * @param target string containing the sequence of characters to match.
+     */
+    public int strStr(String source, String target) {
+        //write your code here
+        if(source == null||target == null)
+            return -1;
+            
+        int index = source.indexOf(target);
+		return index;
+    }
+}
